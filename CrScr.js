5bf6350e792d36061ee68d332df24921762c07c6
Exxar717 <121441127+Exxar717@users.noreply.github.com>
2024-09-07 15:49:13 +0500

    Add files via upload

---
+++ CrScr.js
@@ -0,0 +1,20 @@
+function hideContent(obj) {
+    for (var i = 0; i < obj.length; i++) {
+      document.getElementById([obj[i]]).style.display = "none";
+    }
+    document.getElementById("elChup").style.display = "block";
+  }
+  
+  function hideContent1(obj) {
+    for (var i = 0; i < obj.length; i++) {
+      document.getElementById([obj[i]]).style.display = "none";
+    }
+    document.getElementById("mrHaning").style.display = "block";
+  }
+  
+  function hideContent2(obj) {
+    for (var i = 0; i < obj.length; i++) {
+      document.getElementById([obj[i]]).style.display = "none";
+    }
+    document.getElementById("uLab").style.display = "block";
+  }
